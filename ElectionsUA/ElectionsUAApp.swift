9ef3feb50aa905f818b374acbc6726b11c20f72b
Oksana Fedorchuk <oksana.fedorchuk@requestum.com>
2022-04-20 20:44:16 +0200

    Initial Commit

---
+++ ElectionsUA/ElectionsUAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectionsUAApp.swift
+//  ElectionsUA
+//
+//  Created by Oksana Fedorchuk on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ElectionsUAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

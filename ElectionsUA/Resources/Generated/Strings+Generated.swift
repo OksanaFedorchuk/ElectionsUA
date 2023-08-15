35d6dcdf94cf6161cadfb9c2b050478856917f38
Oksana Fedorchuk <oksanafed.dev@gmail.com>
2023-08-15 23:35:23 +0300

    Added back cocoapods, installed swiftgen, setup swiftgen for strings, refoldering

---
+++ ElectionsUA/Resources/Generated/Strings+Generated.swift
@@ -0,0 +1,280 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+public enum Strings {
+  public enum LawsTab {
+    /// Аналізатор політичної реклами 
+    ///  
+    /// 
+    public static let analizatorLinkString = Strings.tr("LawsTab", "AnalizatorLinkString", fallback: "Аналізатор політичної реклами \n \n")
+    /// Кодекс
+    public static let code = Strings.tr("LawsTab", "Code", fallback: "Кодекс")
+    /// Все про виборчі дані та соцмережі 
+    /// 
+    public static let daniLinkString = Strings.tr("LawsTab", "DaniLinkString", fallback: "Все про виборчі дані та соцмережі \n")
+    /// Депутати
+    public static let deputies = Strings.tr("LawsTab", "Deputies", fallback: "Депутати")
+    /// Виборчий
+    public static let electoral = Strings.tr("LawsTab", "Electoral", fallback: "Виборчий")
+    /// ElectoralCode
+    public static let electoralCode = Strings.tr("LawsTab", "ElectoralCode", fallback: "ElectoralCode")
+    /// Facebook
+    public static let facebook = Strings.tr("LawsTab", "facebook", fallback: "Facebook")
+    /// Обране
+    public static let favourite = Strings.tr("LawsTab", "Favourite", fallback: "Обране")
+    /// Instagram
+    public static let instagram = Strings.tr("LawsTab", "instagram", fallback: "Instagram")
+    /// Закони
+    public static let laws = Strings.tr("LawsTab", "Laws", fallback: "Закони")
+    /// Мапа порушень 
+    /// 
+    public static let mapLinkString = Strings.tr("LawsTab", "MapLinkString", fallback: "Мапа порушень \n")
+    /// NardepyLaw
+    public static let nardepyLaw = Strings.tr("LawsTab", "NardepyLaw", fallback: "NardepyLaw")
+    /// Партії
+    public static let parties = Strings.tr("LawsTab", "Parties", fallback: "Партії")
+    /// PartiesLaw
+    public static let partiesLaw = Strings.tr("LawsTab", "PartiesLaw", fallback: "PartiesLaw")
+    /// Протокол
+    public static let `protocol` = Strings.tr("LawsTab", "Protocol", fallback: "Протокол")
+    /// realm
+    public static let realm = Strings.tr("LawsTab", "realm", fallback: "realm")
+    /// Референдум
+    public static let referendum = Strings.tr("LawsTab", "Referendum", fallback: "Референдум")
+    /// ReferendumLaw
+    public static let referendumLaw = Strings.tr("LawsTab", "ReferendumLaw", fallback: "ReferendumLaw")
+    /// Пошук
+    public static let search = Strings.tr("LawsTab", "Search", fallback: "Пошук")
+    /// Telegram
+    public static let telegram = Strings.tr("LawsTab", "telegram", fallback: "Telegram")
+    /// Twitter
+    public static let twitter = Strings.tr("LawsTab", "twitter", fallback: "Twitter")
+    /// Website
+    public static let website = Strings.tr("LawsTab", "website", fallback: "Website")
+    /// Вебсайт 
+    /// 
+    public static let websiteLinkString = Strings.tr("LawsTab", "WebsiteLinkString", fallback: "Вебсайт \n")
+    /// YouTube
+    public static let youtube = Strings.tr("LawsTab", "youtube", fallback: "YouTube")
+    public enum About {
+      /// Громадянська мережа ОПОРА – 
+      /// 
+      /// 
+      public static let opora = Strings.tr("LawsTab", "About.OPORA", fallback: "Громадянська мережа ОПОРА – \n\n")
+      public enum Opora {
+        /// одна з провідних неурядових та позапартійних всеукраїнських організацій громадського контролю у сфері виборів. З 2007 року ОПОРА аналізує виборчий процес, проводить спостереження за всіма його етапами, працює над удосконаленням виборчого законодавства відповідно до міжнародних стандартів. Загалом за цей час до моніторингу виборів було залучено понад 20 тисяч кваліфікованих спостерігачів. 
+        /// 
+        ///  Для своїх спостерігачів, та усіх інших учасників виборчого процесу ми розробили цей мобільний додаток із виборчим кодексом та верифікатором протоколу, який знадобиться вам у ніч підрахунку голосів. Тепер виборчий кодекс буде завжди із вами, а зручний пошук дозволить швидко знайти необхідну статтю. Отримати усю оперативну інформацію про перебіг виборів в Україні Ви можете тут: 
+        /// 
+        public static let text = Strings.tr("LawsTab", "About.OPORA.Text", fallback: "одна з провідних неурядових та позапартійних всеукраїнських організацій громадського контролю у сфері виборів. З 2007 року ОПОРА аналізує виборчий процес, проводить спостереження за всіма його етапами, працює над удосконаленням виборчого законодавства відповідно до міжнародних стандартів. Загалом за цей час до моніторингу виборів було залучено понад 20 тисяч кваліфікованих спостерігачів. \n\n Для своїх спостерігачів, та усіх інших учасників виборчого процесу ми розробили цей мобільний додаток із виборчим кодексом та верифікатором протоколу, який знадобиться вам у ніч підрахунку голосів. Тепер виборчий кодекс буде завжди із вами, а зручний пошук дозволить швидко знайти необхідну статтю. Отримати усю оперативну інформацію про перебіг виборів в Україні Ви можете тут: \n")
+        public enum Text {
+          /// І не забувайте стежити за нами в соціальних мережах:
+          public static let end = Strings.tr("LawsTab", "About.OPORA.Text.End", fallback: "І не забувайте стежити за нами в соціальних мережах:")
+        }
+      }
+    }
+    public enum Code {
+      public enum Of {
+        /// Кодекс України
+        public static let ukraine = Strings.tr("LawsTab", "Code.Of.Ukraine", fallback: "Кодекс України")
+      }
+    }
+    public enum ElectoralCode {
+      /// ElectoralCode.realm
+      public static let realm = Strings.tr("LawsTab", "ElectoralCode.Realm", fallback: "ElectoralCode.realm")
+    }
+    public enum Facebook {
+      /// https://www.facebook.com/cn.opora
+      public static let link = Strings.tr("LawsTab", "Facebook.Link", fallback: "https://www.facebook.com/cn.opora")
+    }
+    public enum Find {
+      /// ЩОСЬ ЗНАЙТИ?
+      public static let something = Strings.tr("LawsTab", "Find.Something", fallback: "ЩОСЬ ЗНАЙТИ?")
+    }
+    public enum Instagram {
+      /// https://www.instagram.com/cn.opora/
+      public static let link = Strings.tr("LawsTab", "Instagram.Link", fallback: "https://www.instagram.com/cn.opora/")
+    }
+    public enum Law {
+      public enum Of {
+        /// Закон України
+        public static let ukraine = Strings.tr("LawsTab", "Law.Of.Ukraine", fallback: "Закон України")
+      }
+    }
+    public enum NardepyLaw {
+      /// NardepyLaw.realm
+      public static let realm = Strings.tr("LawsTab", "NardepyLaw.Realm", fallback: "NardepyLaw.realm")
+    }
+    public enum Nothing {
+      /// НІЧОГО НЕ ЗНАЙДЕНО
+      public static let found = Strings.tr("LawsTab", "Nothing.Found", fallback: "НІЧОГО НЕ ЗНАЙДЕНО")
+    }
+    public enum On {
+      /// Про вибори народних депутатів України
+      public static let deputies = Strings.tr("LawsTab", "On.Deputies", fallback: "Про вибори народних депутатів України")
+      /// Про політичні партії
+      public static let parties = Strings.tr("LawsTab", "On.Parties", fallback: "Про політичні партії")
+      /// Про всеукраїнський референдум
+      public static let referendum = Strings.tr("LawsTab", "On.Referendum", fallback: "Про всеукраїнський референдум")
+    }
+    public enum PartiesLaw {
+      /// PartiesLaw.realm
+      public static let realm = Strings.tr("LawsTab", "PartiesLaw.Realm", fallback: "PartiesLaw.realm")
+    }
+    public enum ReferendumLaw {
+      /// ReferendumLaw.realm
+      public static let realm = Strings.tr("LawsTab", "ReferendumLaw.Realm", fallback: "ReferendumLaw.realm")
+    }
+    public enum Saved {
+      public enum Tab {
+        /// Saved tab
+        public static let title = Strings.tr("LawsTab", "Saved.Tab.Title", fallback: "Saved tab")
+      }
+    }
+    public enum Search {
+      public enum Tab {
+        /// LawsTab.strings
+        ///   ElectionsUA
+        /// 
+        ///   Created by Oksana Fedorchuk on 21.04.2022.
+        public static let title = Strings.tr("LawsTab", "Search.Tab.Title", fallback: "Search tab")
+      }
+    }
+    public enum Telegram {
+      /// https://t.me/opora_news
+      public static let link = Strings.tr("LawsTab", "Telegram.Link", fallback: "https://t.me/opora_news")
+    }
+    public enum Twitter {
+      /// https://twitter.com/opora
+      public static let link = Strings.tr("LawsTab", "Twitter.Link", fallback: "https://twitter.com/opora")
+    }
+    public enum Website {
+      /// https://oporaua.org/
+      public static let link = Strings.tr("LawsTab", "Website.Link", fallback: "https://oporaua.org/")
+    }
+    public enum YouTube {
+      /// https://www.youtube.com/user/oporavideo
+      public static let link = Strings.tr("LawsTab", "YouTube.Link", fallback: "https://www.youtube.com/user/oporavideo")
+    }
+  }
+  public enum ProtocolTab {
+    public enum Candidate {
+      /// Кандидат 1
+      public static let _1 = Strings.tr("ProtocolTab", "Candidate.1", fallback: "Кандидат 1")
+      /// Кандидат 2
+      public static let _2 = Strings.tr("ProtocolTab", "Candidate.2", fallback: "Кандидат 2")
+    }
+    public enum Error {
+      /// Ти впевнений, що ці бюлетені не підлягають врахуванню? Нагадаємо, не підлягають врахуванню лише наступні бюлетені:
+      ///         - усі бюлетені у виборчій скринці у разі відсутності контрольоного листа;
+      ///         - усі бюлетені у виборчій скринці у разі виникнення сумнівів щодо достовірності контрольного листа
+      ///         -усі бюлетені у переносній виборчій скринці у разі наявності в ній більшої кількості бюлетенів, ніж виборців включених до витягну зі списку виборців
+      ///         -усі бюлетені на яких зазначені номери виборчого округу та дільниці не відповідають номеру відповідного округу;
+      public static let hint = Strings.tr("ProtocolTab", "Error.Hint", fallback: "Ти впевнений, що ці бюлетені не підлягають врахуванню? Нагадаємо, не підлягають врахуванню лише наступні бюлетені:\n        - усі бюлетені у виборчій скринці у разі відсутності контрольоного листа;\n        - усі бюлетені у виборчій скринці у разі виникнення сумнівів щодо достовірності контрольного листа\n        -усі бюлетені у переносній виборчій скринці у разі наявності в ній більшої кількості бюлетенів, ніж виборців включених до витягну зі списку виборців\n        -усі бюлетені на яких зазначені номери виборчого округу та дільниці не відповідають номеру відповідного округу;")
+      public enum _1 {
+        /// Кількість одержаних бюлетенів (п1) не дорівнює сумі кількості невикористаних бюлетенів (п2) Загальній кількості виборців
+        public static let _2 = Strings.tr("ProtocolTab", "Error.1.2", fallback: "Кількість одержаних бюлетенів (п1) не дорівнює сумі кількості невикористаних бюлетенів (п2) Загальній кількості виборців")
+      }
+      public enum _3 {
+        /// Кількість виборців, включених до списку виборців на виборчій дільниці (п.3), менша за загальну кількість виборців, які отримали виборчі бюлетені (п.7)
+        public static let _7 = Strings.tr("ProtocolTab", "Error.3.7", fallback: "Кількість виборців, включених до списку виборців на виборчій дільниці (п.3), менша за загальну кількість виборців, які отримали виборчі бюлетені (п.7)")
+      }
+      public enum _4 {
+        /// Кількість виборців, внесених до витягу із списку виборців для голосування за місцем перебування (п.4), менша за кількість виборців, які отримали виборчі бюлетені за місцем перебування (п.6)
+        public static let _6 = Strings.tr("ProtocolTab", "Error.4.6", fallback: "Кількість виборців, внесених до витягу із списку виборців для голосування за місцем перебування (п.4), менша за кількість виборців, які отримали виборчі бюлетені за місцем перебування (п.6)")
+      }
+      public enum _7 {
+        public enum _5 {
+          /// Кількість виборців, які отримали виборчі бюлетені (п7) не дорівнює сумі кількостей виборців, які отримали виборчі бюлетені у приміщені для голосування (п5) та за місцем перебування (п6)
+          public static let _6 = Strings.tr("ProtocolTab", "Error.7.5.6", fallback: "Кількість виборців, які отримали виборчі бюлетені (п7) не дорівнює сумі кількостей виборців, які отримали виборчі бюлетені у приміщені для голосування (п5) та за місцем перебування (п6)")
+        }
+      }
+      public enum _9 {
+        public enum _11 {
+          /// Кількість виборців які взяли участь у голосуванні(п9) не дорівнює сумі голосів за кандидатів(п 11) та кількості недійсних бюлетенів (п 10)
+          public static let _10 = Strings.tr("ProtocolTab", "Error.9.11.10", fallback: "Кількість виборців які взяли участь у голосуванні(п9) не дорівнює сумі голосів за кандидатів(п 11) та кількості недійсних бюлетенів (п 10)")
+        }
+      }
+    }
+    public enum Total {
+      public enum Ballots {
+        /// ProtocolTab.strings
+        ///   ElectionsUA
+        /// 
+        ///   Created by Oksana Fedorchuk on 30.05.2022.
+        public static let received = Strings.tr("ProtocolTab", "Total.Ballots.Received", fallback: "Кількість виборчих бюлетенів, одержаних дільничною виборчою комісією")
+        /// Кількість невикористаних виборчих бюлетенів, погашених дільничною виборчою комісією
+        public static let unused = Strings.tr("ProtocolTab", "Total.Ballots.Unused", fallback: "Кількість невикористаних виборчих бюлетенів, погашених дільничною виборчою комісією")
+        /// Кількість виборчих бюлетенів, визнаних недійсними
+        public static let unvalid = Strings.tr("ProtocolTab", "Total.Ballots.Unvalid", fallback: "Кількість виборчих бюлетенів, визнаних недійсними")
+        public enum Not {
+          /// Кількість виборчих бюлетенів, що не підлягають врахуванню
+          public static let included = Strings.tr("ProtocolTab", "Total.Ballots.Not.Included", fallback: "Кількість виборчих бюлетенів, що не підлягають врахуванню")
+        }
+      }
+      public enum Voters {
+        /// Кількість виборців, включених до списку виборців на виборчій дільниці
+        public static let enlisted = Strings.tr("ProtocolTab", "Total.Voters.Enlisted", fallback: "Кількість виборців, включених до списку виборців на виборчій дільниці")
+        public enum At {
+          /// Кількість виборців, внесених на виборчій дільниці до витягу із списку виборців для голосування за місцем перебування
+          public static let home = Strings.tr("ProtocolTab", "Total.Voters.At.Home", fallback: "Кількість виборців, внесених на виборчій дільниці до витягу із списку виборців для голосування за місцем перебування")
+        }
+        public enum Received {
+          /// Загальна кількість виборців, які отримали виборчі бюлетені
+          public static let ballots = Strings.tr("ProtocolTab", "Total.Voters.Received.Ballots", fallback: "Загальна кількість виборців, які отримали виборчі бюлетені")
+          public enum Ballots {
+            public enum At {
+              /// Кількість виборців, які отримали виборчі бюлетені за місцем перебування
+              public static let home = Strings.tr("ProtocolTab", "Total.Voters.Received.Ballots.At.Home", fallback: "Кількість виборців, які отримали виборчі бюлетені за місцем перебування")
+              /// Кількість виборців, які отримали виборчі бюлетені у приміщенні для голосування
+              public static let precinct = Strings.tr("ProtocolTab", "Total.Voters.Received.Ballots.At.Precinct", fallback: "Кількість виборців, які отримали виборчі бюлетені у приміщенні для голосування")
+            }
+          }
+        }
+        public enum Voted {
+          public enum At {
+            /// Кількість виборців, які взяли участь у голосуванні на виборчій дільниці
+            public static let precincts = Strings.tr("ProtocolTab", "Total.Voters.Voted.At.Precincts", fallback: "Кількість виборців, які взяли участь у голосуванні на виборчій дільниці")
+          }
+        }
+      }
+    }
+    public enum Verifier {
+      /// Верифікатор протоколів ДВК
+      /// Забудь про уточнені протоколи!
+      /// Перевіряй протоколи ДВК на анявність логічних помилок за допомогою нашого верифікатора. Усе що треба: обрати відповідний тип протоколи та ввести його числові дані. Ми ж у свою чергу, все перевіримо та порахуємо, а у разі виявлення помилок - обов'язково тебе повідомимо, та вкажемо на усі неточності!
+      public static let welcome = Strings.tr("ProtocolTab", "Verifier.Welcome", fallback: "Верифікатор протоколів ДВК\nЗабудь про уточнені протоколи!\nПеревіряй протоколи ДВК на анявність логічних помилок за допомогою нашого верифікатора. Усе що треба: обрати відповідний тип протоколи та ввести його числові дані. Ми ж у свою чергу, все перевіримо та порахуємо, а у разі виявлення помилок - обов'язково тебе повідомимо, та вкажемо на усі неточності!")
+    }
+  }
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension Strings {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
